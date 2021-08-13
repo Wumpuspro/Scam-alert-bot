d284ea018fa629eedd6f9903ce7fd37b98b17a1a
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-13 21:51:53 +0545

    Create caseinfo.js

---
+++ commands/scam-alert/caseinfo.js
@@ -0,0 +1,43 @@
+const Scam = require("../Schema/ScamSchema");
+const { Client, Message, MessageEmbed } = require("discord.js");
+var config = require("../../config/config.json");
+const moment = require("moment")
+
+module.exports = {
+  name: "scam-alert",
+  aliases: ["case-info", "uidinfo", "uid-info"],
+  categories: "other",
+  permissions: " ",
+  description: "Send information about case",
+  usage: "",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+  let CaseInfo = args[0];
+  if (!CaseInfo) return message.channel.send(`**Please Provide The Case UID**`);
+
+  let UidInfo = await Scam.findOne({ CaseUID: CaseInfo });
+  if (!UidInfo)
+    return message.channel.send(`**No Case Found With The Provided UID**`);
+
+  let embed = new discord.MessageEmbed()
+    .setTitle(`Scam Details`)
+    .setDescription(
+      `
+-----------------------------------------------------------------------
+**__Case Reporter Was__** : \`${UidInfo.Reporter}\`
+ **__Reported Person Name / Scammer Was__** : \`${UidInfo.ScammerName}\`
+ **__Reported Person ID / Scammer ID Was__** : \`${UidInfo.ScammerID}\`
+ **__The Description Provided__** : \`${UidInfo.ScamReason}\`
+ **__Scammer Server Details__** : \`${UidInfo.ServerDetails}\`
+**__Some Attachments__** : ${UidInfo.Attachment}
+-----------------------------------------------------------------------
+`
+    )
+    .setColor("RANDOM")
+    .setFooter(`ScamAlert `);
+  message.channel.send(embed);
+};
